ceea4d52652eaf1266d2ebc2f86881d4969d5a4c
AbdullahAli16 <abdullah.ali.devx@gmail.com>
2026-09-12 20:28:43 +0500

    Cleanup: remove test files, debug scripts, SQL dumps, docs, backup htaccess files, and frontend temp files

--- inspect_je.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$columns = \Illuminate\Support\Facades\Schema::getColumnListing('journal_entries');
-print_r($columns);
--- scratch_check_schema.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
-$res = DB::select('describe journal_entries');
-print_r($res);
